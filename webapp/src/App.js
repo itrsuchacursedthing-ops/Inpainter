b39c9bb8a26ae33698e05081fa74ea3c50c88187
itrsuchacursedthing-ops <itrsuchacursedthing@gmail.com>
2025-07-24 19:38:24 +0700

    New IP

--- webapp/src/App.js
+++ webapp/src/App.js
@@ -56,7 +56,7 @@ function App() {
   const [result, setResult] = useState(null);
   const [loading, setLoading] = useState(false);
 
-  const BACKEND_URL = 'https://expensively-accredited-clingfish.cloudpub.ru/';
+  const BACKEND_URL = 'https://warily-benevolent-bunting.cloudpub.ru/';
 
   const handleImageChange = (e) => {
     const file = e.target.files[0];
